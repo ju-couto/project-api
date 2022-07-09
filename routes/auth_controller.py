340fb04375b504f65057ffd45bebdae3be8a4a87
jucouto <juliamartinscouto0403@gmail.com>
2022-07-09 20:09:37 -0300

    feat: token generator and login implementation

--- routes/auth_controller.py
+++ routes/auth_controller.py
@@ -4,7 +4,7 @@ from typing import List, Optional
 from db import get_db, Session
 from sql_app.user_repo import UserRepo
 from sql_app import schemas
-import security
+from security import get_password_hash, verify_password, create_access_token, logged_user
 
 router = APIRouter()
 
@@ -12,12 +12,12 @@ router = APIRouter()
 @router.post('/sigup', response_model=schemas.User, status_code=201)
 async def sign_up(user_request: schemas.UserCreate, db: Session = Depends(get_db)):
     """
-    Create a new user\f
+    Create a new user
     """
     db_user = UserRepo.fetch_by_username(db, username=user_request.username)
     if db_user:
         raise HTTPException(status_code=400, detail='This username already exists!')
-    user_request.password = security.get_password_hash(user_request.password)
+    user_request.password = get_password_hash(user_request.password)
     return await UserRepo.create(db=db, user=user_request)
 
 
@@ -30,12 +30,17 @@ def login(login_request: schemas.Login, db: Session = Depends(get_db)):
 
     if not user_db:
         raise HTTPException(status_code=400, detail='Incorrect username or password!')
-    valid_password = security.verify_password(login_request.password, user_db.password)
+    valid_password = verify_password(login_request.password, user_db.password)
 
     if not valid_password:
         raise HTTPException(status_code=400, detail='Incorrect username or password!')
+    token = create_access_token({'sub': user_db.username})
+    return {'user': user_db, 'access_token': token}
 
-    return user_db
+
+@router.get('/me')
+def me(user: schemas.User = Depends(logged_user), db: Session = Depends(get_db)):
+    return user
 
 
 @router.get('/users', response_model=List[schemas.User])
--- security.py
+++ security.py
@@ -1,8 +1,17 @@
 from passlib.context import CryptContext
-from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from jose import jwt, JWTError
+from datetime import datetime, timedelta
+from fastapi.security import OAuth2PasswordBearer
+from fastapi import Depends, HTTPException
+from db import Session, get_db
+from sql_app.user_repo import UserRepo
 
 pwd_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl='token')
+oauth2_schema = OAuth2PasswordBearer(tokenUrl='token')
+# CONFIG
+SECRET_KEY = '43DDC799099616CC04D43815AAD5694B0768F7C66F2D1F193C2017786E7D3FB5'
+ALGORITHM = 'HS256'
+EXPIRES_IN_MIN = 5
 
 
 def verify_password(plain_password: str, hashed_password: str):
@@ -13,3 +22,32 @@ def get_password_hash(password: str) -> str:
     return pwd_context.hash(password)
 
 
+def create_access_token(data: dict):
+    data_copy = data.copy()
+    expiration = datetime.utcnow() + timedelta(minutes=EXPIRES_IN_MIN)
+
+    data_copy.update({'exp': expiration})
+
+    token_jwt = jwt.encode(data_copy, SECRET_KEY, algorithm=ALGORITHM)
+    return token_jwt
+
+
+def verify_access_token(token: str):
+    payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+    return payload.get('sub')
+
+
+def logged_user(token: str = Depends(oauth2_schema), db: Session = Depends(get_db)):
+    exception = HTTPException(status_code=401, detail='Invalid token!')
+    try:
+        username = verify_access_token(token)
+    except JWTError:
+        raise exception
+
+    if not username:
+        raise exception
+    db_user = UserRepo.fetch_by_username(db, username=username)
+
+    if not db_user:
+        raise exception
+    return db_user
